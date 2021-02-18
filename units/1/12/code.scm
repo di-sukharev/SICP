b63818577fc8d45b5752bcf92759c91ea43d63a6
dmitry.sukharev <dmitry.sukharev@alibaba-inc.com>
2021-02-18 16:27:18 +0300

    dont understand 1.12

---
+++ units/1/12/code.scm
@@ -0,0 +1,12 @@
+ (define (pascal row col) 
+   (print row " " col)
+   (cond ((or (< row col)  
+              (< col 1)) 0 ) 
+         ((or (= col 1) 
+              (= col row)) 1) 
+         (else (+ (pascal (- row 1) (- col 1)) 
+                  (pascal (- row 1) col ))))) 
+
+(define (main args)
+   (print (pascal (string->number (car args))
+                  (string->number (cadr args)))))
