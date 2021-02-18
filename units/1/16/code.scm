6aea540334011905d71428c0af1c5102c8f9075b
Dima <dima@sukharev.io>
2021-02-18 22:28:45 +0300

    solved 1.16

---
+++ units/1/16/code.scm
@@ -0,0 +1,13 @@
+(define (power b n)
+   (define (iter B N A) ; (iter B N A) = B^N * A
+      (print B " " N " " A)
+      (cond ((= 0 N) A) 
+            ((even? N) (iter (square B) (/ N 2) A)) 
+            (else (iter B (- N 1) (* B A))))) 
+   (iter b n 1)) 
+
+(define (square x) (* x x))
+
+(define (main args)
+   (print (power (string->number (car args))
+                     (string->number (cadr args)))))
