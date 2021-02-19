2080a6cd82eed3f9cd2f1575095043d682626ffb
dmitry.sukharev <dmitry.sukharev@alibaba-inc.com>
2021-02-19 23:20:32 +0300

    1.22 solved

--- units/1/22/code.scm
+++ units/1/22/code.scm
@@ -1,15 +1,42 @@
-(define (timed-prime-test n)
-   (newline)
-   (display n)
-   (start-prime-test n (runtime))) ; chicken scheme doesn't have runtime =(
+(define runtime 0) ; chicken scheme doesn't have runtime =(
+
+(define (divides? a b)
+   (= (remainder b a) 0))
+
+(define (square x) (* x x))
+
+(define (find-divisor n test)
+   (cond ((> (square test) n) n)
+         ((divides? test n) test)
+         (else (find-divisor n (+ test 1)))))
+
+(define (smallest-divisor n)
+   (find-divisor n 2))
+
+(define (report-prime elapse-time)
+   (display " 🌞 is prime 🌞 ")
+   (display elapse-time))
+
+(define (prime? n)
+   (= n (smallest-divisor n)))
 
 (define (start-prime-test n start-time)
    (if (prime? n)
-       (report-prime (- (runtime) start-time))))
+       (report-prime (- runtime start-time))))
 
-(define (report-prime elapse-time)
-   (display " *** ")
-   (display elapsed-time))
+(define (timed-prime-test n)
+   (newline)
+   (display n)
+   (start-prime-test n runtime))
+
+(define (search-for-primes from to)
+   (define (iter from to)
+      (if (<= from to)
+          ((timed-prime-test from) (iter (+ from 1) to))))
+
+   (iter from to))
+
 
 (define (main args)
-   (print (timed-prime-test (string->number (car args)))))
+   (print (search-for-primes (string->number (car args))
+                             (string->number (cadr args)))))
