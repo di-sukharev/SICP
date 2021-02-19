c60189c6db1167fe6086e1d0e61b91b7b7c64606
dmitry.sukharev <dmitry.sukharev@alibaba-inc.com>
2021-02-19 16:35:52 +0300

    removed empty whitespaces

--- units/1/19/code.scm
+++ units/1/19/code.scm
@@ -3,18 +3,18 @@
 (define (fib-iter a b p q count) 
    (cond ((= count 0) b) 
          ((even? count) 
-          (fib-iter a 
-                    b 
-                    (+ (square p) (square q)) 
-                    (+ (* 2 p q) (square q)) 
-                    (/ count 2))) 
-         (else (fib-iter (+ (* b q) (* a q) (* a p)) 
-                         (+ (* b p) (* a q)) 
-                         p 
-                         q 
-                         (- count 1))))) 
+          (fib-iter a
+                    b
+                    (+ (square p) (square q))
+                    (+ (* 2 p q) (square q))
+                    (/ count 2)))
+         (else (fib-iter (+ (* b q) (* a q) (* a p))
+                         (+ (* b p) (* a q))
+                         p
+                         q
+                         (- count 1)))))
   
-(define (square x) (* x x)) 
+(define (square x) (* x x))
   
 
 (define (main args)
