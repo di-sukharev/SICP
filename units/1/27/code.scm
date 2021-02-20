d0082634901a12989a9747b549e32e9d494af652
dmitry.sukharev <dmitry.sukharev@alibaba-inc.com>
2021-02-20 21:36:08 +0300

    rewrited 1.27 from * to square, forgotten from 1.26

--- units/1/27/code.scm
+++ units/1/27/code.scm
@@ -7,8 +7,7 @@
 
 (define (expmod base exp m)
    (cond ((= exp 0) 1)
-         ((even? exp) (remainder (* (expmod base (/ exp 2) m)
-                                    (expmod base (/ exp 2) m))
+         ((even? exp) (remainder (square (expmod base (/ exp 2) m))
                                  m))
          (else (remainder (* base (expmod base (- exp 1) m)) m))))
 
